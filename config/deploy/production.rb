317575351831c6968927ba6a29b3ca2261422a2a
daisuke <michiue.daisuke@gmail.com>
2019-05-03 20:17:13 +0900

    make hometop

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,12 @@
 # conohaのサーバーのIP、ログインするユーザー名、サーバーの役割
 # xxxの部分はサーバーのIPアドレス
 # 10022はポートを変更している場合。通常は22
-server '118.27.35.203', user: 'michiue', roles: %w{app db web}, port: 10022 
+server '118.27.35.203', user: 'hoge', roles: %w{app db web}, port: 10022 
 
 #デプロイするサーバーにsshログインする鍵の情報。サーバー編で作成した鍵のパス
-set :ssh_options, keys: '~/.ssh/conoha_hoge/id_rsa'
+set :ssh_options, {
+	port: 10022,
+	forward_agent: true,
+	keys: [File.expand_path('~/.ssh/id_rsa')],
+	auth_methods: %w(publickey)
+}
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'users#index'
+  get 'home/top' => 'home#top'
 end
