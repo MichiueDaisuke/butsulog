5a3665b2cc834258d53fd3cdb115a076898ba910
daisuke <michiue.daisuke@gmail.com>
2019-05-08 17:25:40 +0900

    特にありません

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,16 +4,21 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post=Post.find_by(id: params[:id])
+    @post = Post.find_by(id: params[:id])
   end
 
   def new
+    @post=Post.new
   end
 
   def create
-    post = Post.new(reputation: params[:reputation])
-    post.save
-    redirect_to("/posts/index")
+    @post = Post.new(reputation: params[:reputation])
+    if @post.save
+      flash[:notice]="投稿を作成しました"
+      redirect_to("/posts/index")
+    else
+      render("posts/new")
+    end
   end
 
   def edit
@@ -21,19 +26,20 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post=Post.find_by(id: params[:id])
-    @post.reputation=params[:reputation]
+    @post = Post.find_by(id: params[:id])
+    @post.reputation = params[:reputation]
     if @post.save
-    redirect_to("/posts/index")
-
+      flash[:notice] = "投稿を編集しました"
+      redirect_to("/posts/index")
     else
       render("posts/edit")
-      end
+    end
   end
 
   def destroy
-    @post= Post.find_by(id: params[:id])
+    @post = Post.find_by(id: params[:id])
     @post.destroy
+    flash[:notice]="投稿を削除しました"
     redirect_to("/posts/index")
   end
 end
