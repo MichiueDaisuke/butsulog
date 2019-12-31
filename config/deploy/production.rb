1010cf34ff27d3fb9d7f50d3fa2857bd94713f6c
daisuke <michiue.daisuke@gmail.com>
2020-01-01 01:12:14 +0900

    test

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,12 +1,12 @@
 # conohaのサーバーのIP、ログインするユーザー名、サーバーの役割
 # xxxの部分はサーバーのIPアドレス
 # 10022はポートを変更している場合。通常は22
-server '118.27.35.203', user: 'hoge', roles: %w{app db web}, port: 10022 
+server '118.27.19.21', user: 'hoge', roles: %w{app db web}, port: 10022 
 
 #デプロイするサーバーにsshログインする鍵の情報。サーバー編で作成した鍵のパス
 set :ssh_options, {
 	port: 10022,
-	forward_agent: true,
+	forward_agent: false,
 	keys: [File.expand_path('~/.ssh/id_rsa')],
 	auth_methods: %w(publickey)
 }
